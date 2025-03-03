05f92a9e651e413880436af1aae72105eb0021a2
Pierre Bressy <pierre.bressy@heig-vd.ch>
2025-03-03 14:20:52 +0000

    v1

---
+++ TD20250303-bis/td.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+double deg2rad(const double d) {
+	return d*M_PI/180.0;
+}
+int main(int argc, const char *argv[])
+{
+	const char *filename = "./data/circle.txt"; // rel
+	const char *mode = "w";					  // r = read
+	FILE *f = NULL;
+
+	f = fopen(filename, mode);
+	if (NULL == f)
+	{
+		printf("Error while opening %s\n", filename);
+		return 1;
+	}
+
+	const double ALPHA_MIN=0.;
+	const double ALPHA_MAX=359.;
+	const double ALPHA_STEP=1.;
+	const double RADIUS=2.0;
+	for(double alpha=ALPHA_MIN;alpha<=ALPHA_MAX;alpha+=ALPHA_STEP) {
+		double r=RADIUS*cos(deg2rad(alpha));
+		double i=RADIUS*sin(deg2rad(alpha));
+		fprintf(f,"%5.2lf %+8.3lf %+8.3lf\n",alpha,r,i);
+	}
+	if (0 != fclose(f))
+	{
+		printf("Error while closing %s\n", filename);
+		return 2;
+	}
+
+	return 0;
+}
