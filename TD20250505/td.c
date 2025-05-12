73e73588c9fd83458b0e40ef682be3211ce95244
Pierre Bressy <pierre.bressy@heig-vd.ch>
2025-05-12 12:22:34 +0000

    update TD20250505

--- TD20250505/td.c
+++ TD20250505/td.c
@@ -8,28 +8,31 @@ int main(int argc, const char *argv[])
 	display_list(l);
 
 	puts("insert elem at pos 0");
-	elem e1={.x=1., .y=2.};
+	elem e1 = {.x = 1., .y = 2.};
 	insert_list(&l, e1, 0);
 	display_list(l);
 	puts("insert elem at pos 1");
-	elem e2={.x=5., .y=6.};
+	elem e2 = {.x = 5., .y = 6.};
 	insert_list(&l, e2, 1);
 	display_list(l);
-	for(int k=0;k<30;k++) {
+	for (int k = 0; k < 30; k++)
+	{
 		puts("insert elem at pos 1");
-		elem e3={.x=3., .y=4.};
+		elem e3 = {.x = 3., .y = 4.};
 		insert_list(&l, e3, 1);
-	
 	}
 	display_list(l);
 
-	//remove_list(&l,0);
-	//display_list(l);
+	// remove_list(&l,0);
+	// display_list(l);
 
 	puts("--- elem at pos 0");
 	elem e4;
-	get_elem(l,0,&e4);
+	get_elem(l, 0, &e4);
 	display_elem(e4);
+
+	remove_list(&l, 0);
+	display_list(l);
 
 	return 0;
 }
